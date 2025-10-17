d8a33d19b4c229b1609588885c3ae51d1e981b60
v-tadic <vukvukvukt@gmail.com>
2025-10-17 21:01:13 +0200

    Added some comments

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,12 @@ import Playlist from './components/playlist'
 
 function App() {
 
+    //state za ime plejliste
     const [playlistName, setPlaylistName] = useState('')
+    //state za proveru da li je track u plejlisti
     const [trackIsInPlaylist, setTrackIsInPlaylist] = useState()
 
+    //privremeno - simuliranje pesama koje smo dobili na osnovu search
     const [tracks, setTracks] = useState([
         { id: '1', name: 'Bass', artist: 'Jala Brat', uri:'spotify:track:54AUuzGDu5BEbUGY5FWd9k'},
         { id: '2', name: 'Time', artist: 'Pink Floyd', uri:'spotify:track:3TO7bbrUKrOSPGRTB5MeCz'},
@@ -17,11 +20,12 @@ function App() {
         { id: '5', name: 'Sprinter', artist: 'Central Cee', uri:'spotify:track:2FDTHlrBguDzQkp7PVj16Q'}
     ]);
 
+    //state koji cuva podatke o pesmama u plejlisti u vidi array objekata
     const [playlistTracks, setPlaylistTracks] = useState([
 
     ]);
 
-
+    //funcija koja uzima objekat sa podatcima o tracku i dodaje ga u playlistTracks
     function addTrackToPlaylist(track) {
         const existsInArray = playlistTracks.some(oldTrack => oldTrack.name === track.name)
         if(!existsInArray) {
@@ -29,6 +33,7 @@ function App() {
         }
     }
 
+    //funkcija koja uzima objekat sa podatcima o track i taj track "uklanja iz plejliste" !
     function removeTrack(track) {
         const existsInPlaylist = playlistTracks.some(allTrack => allTrack.name === track.name)
         if(existsInPlaylist) {
@@ -36,13 +41,13 @@ function App() {
         }
     }
 
+    //funkcija koja stvara novi array stringova, ovaj array cuva uri svih pesama iz playlistTracks
     function addPlaylistToUserAccount(){
         let uriArray = playlistTracks.map(track => track.uri);
         console.log(uriArray);
         setPlaylistTracks([]);
         setPlaylistName('');
     }
-
 
   return (
     <div>
