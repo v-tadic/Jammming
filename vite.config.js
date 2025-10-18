6c43617335e3e081c48408b79e5181c480f5c89e
v-tadic <vukvukvukt@gmail.com>
2025-10-18 15:55:35 +0200

    Small changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        host: '127.0.0.1',
+        port: 5173,
+    },
 })
