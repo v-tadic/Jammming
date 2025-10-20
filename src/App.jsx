2c8b11128bd75b0021e73ea56e645a6940c90e89
v-tadic <vukvukvukt@gmail.com>
2025-10-20 22:20:18 +0200

    Implemented getting the code from spotify

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,117 @@
-import { useState, useEffect } from 'react'
-import styles from './app.module.css'
-import Search from '../src/components/search'
-import Track from './components/track'
-import SearchResults from './components/searchResults'
-import Playlist from './components/playlist'
+import { useState, useEffect } from 'react';
+import styles from './app.module.css';
+import Search from './components/search';
+import Track from './components/track';
+import SearchResults from './components/searchResults';
+import Playlist from './components/playlist';
+import playlist from "./components/playlist";
 
 function App() {
-
-    //state za ime plejliste
-    const [playlistName, setPlaylistName] = useState('')
-    //state za proveru da li je track u plejlisti
-    const [trackIsInPlaylist, setTrackIsInPlaylist] = useState()
-
-    //privremeno - simuliranje pesama koje smo dobili na osnovu search
+    const [playlistName, setPlaylistName] = useState('');
+    const [trackIsInPlaylist, setTrackIsInPlaylist] = useState();
+    const [clientId, setClientId] = useState('e01a6d79177d4951a630fc9014f5c482');
+    const [redirectUri, setRedirectUri] = useState('http://127.0.0.1:5173');
     const [tracks, setTracks] = useState([
-        { id: '1', name: 'Bass', artist: 'Jala Brat', uri:'spotify:track:54AUuzGDu5BEbUGY5FWd9k'},
-        { id: '2', name: 'Time', artist: 'Pink Floyd', uri:'spotify:track:3TO7bbrUKrOSPGRTB5MeCz'},
-        { id: '3', name: 'Freebird', artist: 'Lynyrd Skynyrd', uri:'spotify:track:5EWPGh7jbTNO2wakv8LjUI'},
-        { id: '5', name: 'Sprinter', artist: 'Central Cee', uri:'spotify:track:2FDTHlrBguDzQkp7PVj16Q'}
-    ]);
-
-    //state koji cuva podatke o pesmama u plejlisti u vidi array objekata
-    const [playlistTracks, setPlaylistTracks] = useState([
 
     ]);
 
-    //funcija koja uzima objekat sa podatcima o tracku i dodaje ga u playlistTracks
+    const [playlistTracks, setPlaylistTracks] = useState([]);
+
+    //Funkcije za generisanje code verifier i code challenge
+    const generateRandomString = (length) => {
+        const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const values = crypto.getRandomValues(new Uint8Array(length));
+        return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+    }
+    const sha256 = async (plain) => {
+        const encoder = new TextEncoder()
+        const data = encoder.encode(plain)
+        return window.crypto.subtle.digest('SHA-256', data)
+    }
+    const base64encode = (input) => {
+        return btoa(String.fromCharCode(...new Uint8Array(input)))
+            .replace(/=/g, '')
+            .replace(/\+/g, '-')
+            .replace(/\//g, '_');
+    }
+
+    async function ObtainCodeChallenge() {
+        const codeVerifier  = generateRandomString(64);
+        window.localStorage.setItem('codeVerifier', codeVerifier);
+        const hashed = await sha256(codeVerifier)
+        const codeChallenge = base64encode(hashed);
+        window.localStorage.setItem('codeChallenge', codeChallenge);
+    }
+
+    async function RequestUserAuthorization(){
+        await ObtainCodeChallenge()
+        const scope = 'user-read-private user-read-email';
+        const authUrl = new URL("https://accounts.spotify.com/authorize")
+
+        const params =  {
+            response_type: 'code',
+            client_id: clientId,
+            scope,
+            code_challenge_method: 'S256',
+            code_challenge: window.localStorage.getItem('code_challenge'),
+            redirect_uri: redirectUri,
+        }
+
+        authUrl.search = new URLSearchParams(params).toString();
+        window.location.href = authUrl.toString();
+    }
+
+    async function HandleRedirect() {
+        const urlParams = new URLSearchParams(window.location.search);
+        let code = urlParams.get('code');
+
+        if (code) {
+            window.localStorage.setItem('code', code);
+        }
+    }
+
+    if (new URLSearchParams(window.location.search).has('code')) {
+        HandleRedirect();
+    }
+
     function addTrackToPlaylist(track) {
-        const existsInArray = playlistTracks.some(oldTrack => oldTrack.name === track.name)
-        if(!existsInArray) {
+        const existsInArray = playlistTracks.some((oldTrack) => oldTrack.name === track.name);
+        if (!existsInArray) {
             setPlaylistTracks((prev) => [...prev, track]);
         }
     }
 
-    //funkcija koja uzima objekat sa podatcima o track i taj track "uklanja iz plejliste" !
     function removeTrack(track) {
-        const existsInPlaylist = playlistTracks.some(allTrack => allTrack.name === track.name)
-        if(existsInPlaylist) {
-            setPlaylistTracks(playlistTracks.filter(allTrack => allTrack.name !== track.name))
+        const existsInPlaylist = playlistTracks.some((allTrack) => allTrack.name === track.name);
+        if (existsInPlaylist) {
+            setPlaylistTracks(playlistTracks.filter((allTrack) => allTrack.name !== track.name));
         }
     }
 
-    //funkcija koja stvara novi array stringova, ovaj array cuva uri svih pesama iz playlistTracks
-    function addPlaylistToUserAccount(){
-        let uriArray = playlistTracks.map(track => track.uri);
+    function addPlaylistToUserAccount() {
+        let uriArray = playlistTracks.map((track) => track.uri);
         console.log(uriArray);
         setPlaylistTracks([]);
         setPlaylistName('');
     }
 
-  return (
-    <div>
-      <h1 className={styles.Title}>Jammming</h1>
-      <Search></Search>
-      <SearchResults tracks={tracks} addTrackToPlaylist={addTrackToPlaylist}></SearchResults>
-      <h1 className={styles.YourPlaylist}>Your Playlist:</h1>
-      <Playlist addPlaylistToUserAccount={addPlaylistToUserAccount} playlistName={playlistName} setPlaylistName={setPlaylistName} playlistTracks={playlistTracks} removeTrack={removeTrack} trackIsInPlaylist={trackIsInPlaylist}></Playlist>
-    </div>
-  )
+    return (
+        <div>
+            <h1 className={styles.Title}>Jammming</h1>
+            <button onClick={RequestUserAuthorization}>Log In With Spotify</button>
+            <Search></Search>
+            <SearchResults tracks={tracks} addTrackToPlaylist={addTrackToPlaylist}></SearchResults>
+            <h1 className={styles.YourPlaylist}>Your Playlist:</h1>
+            <Playlist
+                addPlaylistToUserAccount={addPlaylistToUserAccount}
+                playlistName={playlistName}
+                setPlaylistName={setPlaylistName}
+                playlistTracks={playlistTracks}
+                removeTrack={removeTrack}
+                trackIsInPlaylist={trackIsInPlaylist}
+            />
+        </div>
+    );
 }
 
-export default App
+export default App;
